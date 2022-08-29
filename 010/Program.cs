3201e7c01385dc579d3346e9888f4fac5f73e303
Hanna Pon <hannainleto@mail.ru>
2022-08-29 18:28:39 +0300

    Change file

--- 010/Program.cs
+++ 010/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Write("Введи трёхзначное число: ");
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введи трёхзначное число: ");
 int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(threeDigitNumber);
 Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
